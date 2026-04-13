f6221b13b8216f9616348e9217d5e06d6af2b9c5
Jake Caden Roll <rolljake@msu.edu>
2026-04-13 09:39:48 -0400

    Create model.py with function stubs for training and evaluating a classification model

---
+++ src/model.py
@@ -0,0 +1,34 @@
+def train_model(X_train, y_train):
+    """
+    Trains a classification model using the provided training data.
+
+    Parameters:
+    X_train : array-like
+        Feature data used for training the model.
+    y_train : array-like
+        Target labels corresponding to the training data.
+
+    Returns:
+    model : Trained model object
+        The trained classification model.
+    """
+    pass
+
+
+def evaluate_model(model, X_test, y_test):
+    """
+    Evaluates the performance of the trained classification model on the test data.
+
+    Parameters:
+    model : Trained model object
+        The classification model to evaluate.
+    X_test : array-like
+        Feature data used for testing the model.
+    y_test : array-like
+        Target labels corresponding to the test data.
+
+    Returns:
+    metrics : dict
+        A dictionary containing evaluation metrics such as accuracy, precision, recall, etc.
+    """
+    pass
